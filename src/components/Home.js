bca98da5b1606e39a3e3c6fe952b6e64a23c2c21
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-04-05 12:46:31 +0200

    [UPDATE] To hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,90 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { TodoForm } from './TodoForm';
 import { List } from './List';
 import { getTodos, addTodo, removeTodo, updateTodo } from '../http/todos';
 
-export class Home extends Component {
-  constructor(props) {
-    super(props);
+export function Home() {
+  const [todos, setTodos] = useState([]);
 
-    this.state = {
-      todos: []
-    };
-
-    this.handleAddTodo = this.handleAddTodo.bind(this);
-    this.handleRemoveTodo = this.handleRemoveTodo.bind(this);
-    this.handleCompleteTodo = this.handleCompleteTodo.bind(this);
-  }
-
-  async componentDidMount() {
-    const { data: todos } = await getTodos();
-
-    if (todos) {
-      this.setState({
-        todos
-      });
+  useEffect(() => {
+    async function fetchTodos() {
+      const { data } = getTodos();
+      if (data) {
+        setTodos(data);
+      }
     }
-  }
+    fetchTodos();
+  }, []);
 
-  async handleAddTodo(todo) {
+  const handleAddTodo = useCallback(async todo => {
     const { data } = await addTodo({
       ...todo,
       createdAt: new Date(),
       completed: false
     });
 
-    this.setState({ todos: [data, ...this.state.todos] });
-  }
+    setTodos([data, ...todos]);
+  });
 
-  async handleRemoveTodo(id) {
+  const handleRemoveTodo = useCallback(async id => {
     await removeTodo(id);
 
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    });
-  }
+    setTodos(todos.filter(todo => todo.id !== id));
+  });
 
-  async handleCompleteTodo(id) {
-    const updatedTodo = this.state.todos.find(todo => todo.id === id);
+  const handleCompleteTodo = useCallback(async id => {
+    const updatedTodo = todos.find(todo => todo.id === id);
     updatedTodo.completed = true;
 
     await updateTodo(id, updatedTodo);
 
-    this.setState({
-      todos: this.state.todos.map(todo => {
+    setTodos(
+      todos.map(todo => {
         if (todo.id === id) {
           todo.completed = true;
         }
         return todo;
       })
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <TodoForm onAddTodo={this.handleAddTodo} />
-        <div className="row mt-5">
-          <div className="col">
-            <h3>My Task List</h3>
-            <hr />
-            <List
-              items={this.state.todos.filter(todo => !todo.completed)}
-              onComplete={this.handleCompleteTodo}
-              onRemove={this.handleRemoveTodo}
-            />
-          </div>
-          <div className="col">
-            <h3>Completed</h3>
-            <hr />
-            <List
-              items={this.state.todos.filter(todo => todo.completed)}
-              onComplete={this.handleCompleteTodo}
-              onRemove={this.handleRemoveTodo}
-            />
-          </div>
-        </div>
-      </React.Fragment>
     );
-  }
+  });
+
+  return (
+    <React.Fragment>
+      <TodoForm onAddTodo={handleAddTodo} />
+      <div className="row mt-5">
+        <div className="col">
+          <h3>My Task List</h3>
+          <hr />
+          <List
+            items={todos.filter(todo => !todo.completed)}
+            onComplete={handleCompleteTodo}
+            onRemove={handleRemoveTodo}
+          />
+        </div>
+        <div className="col">
+          <h3>Completed</h3>
+          <hr />
+          <List
+            items={todos.filter(todo => todo.completed)}
+            onComplete={handleCompleteTodo}
+            onRemove={handleRemoveTodo}
+          />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
